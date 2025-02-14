9f0ca84f43bf8ccff361c947b1ea8581fd769be7
Paul Boes <pvboes@gmail.com>
2025-02-14 13:38:22 +0100

    remove homegroup process for now

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -7,12 +7,12 @@ module.exports = {
         exec_mode: "fork", 
         node_args: "--env-file .env.cmg"
       },
-      {
-        name: "arbbot_homegroup",
-        script: "dist/index.js",
-        instances: 1,
-        exec_mode: "fork",
-        node_args: "--env-file .env.homegroup"
-      },
+      // {
+      //   name: "arbbot_homegroup",
+      //   script: "dist/index.js",
+      //   instances: 1,
+      //   exec_mode: "fork",
+      //   node_args: "--env-file .env.homegroup"
+      // },
     ]
   };
