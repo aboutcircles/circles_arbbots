b2880931ae9873d0aeafde8a13b12ad9806e4f9c
Paul Boes <pvboes@gmail.com>
2025-03-05 12:12:39 +0100

    typo in ecosystem config

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,18 +1,18 @@
 module.exports = {
     apps: [
       {
-        name: "arbbot_cms",
+        name: "arbbot_cmg",
         script: "dist/index.js", 
         instances: 1, 
         exec_mode: "fork", 
         node_args: "--env-file .env.cmg"
       },
-      // {
-      //   name: "arbbot_homegroup",
-      //   script: "dist/index.js",
-      //   instances: 1,
-      //   exec_mode: "fork",
-      //   node_args: "--env-file .env.homegroup"
-      // },
+      {
+        name: "arbbot_metestsup",
+        script: "dist/index.js",
+        instances: 1,
+        exec_mode: "fork",
+        node_args: "--env-file .env.metestsup"
+      },
     ]
   };
