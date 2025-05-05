941532b83e2967a87712c1601ca577086b8233b7
Paul Boes <pvboes@gmail.com>
2025-05-05 16:38:09 +0200

    truning group-specific files into specific subfolder

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,17 +2,17 @@ module.exports = {
   apps: [
     {
       name: "arbbot_metestsup",
-      script: "dist/index.js",
+      script: "dist/group-specific/index.js",
       instances: 1,
       exec_mode: "fork",
-      node_args: "--env-file .env.metestsup",
+      node_args: "--env-file env_files/.env.metestsup",
     },
     {
       name: "arbbot_bfn",
       script: "dist/index.js",
       instances: 1,
       exec_mode: "fork",
-      node_args: "--env-file .env.bfn",
+      node_args: "--env-file env_files/.env.bfn",
     },
   ],
 };
